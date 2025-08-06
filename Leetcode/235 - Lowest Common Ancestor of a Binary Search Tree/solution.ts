72b3943256c07480420b3392a0f3f4c56f81b070
Harshit Sharma <sharma.harshit0059@gmail.com>
2025-08-06 15:26:18 +0530

    Leetcode | Problem 235 added with solutions

---
+++ Leetcode/235 - Lowest Common Ancestor of a Binary Search Tree/solution.ts
@@ -0,0 +1,12 @@
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	while (root !== null) {
+        if (p.val < root.val && q.val < root.val) {
+            root = root.left;
+        } else if (p.val > root.val && q.val > root.val) {
+            root = root.right;
+        } else {
+            return root;
+        }
+    }
+    return null;
+};
