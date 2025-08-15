3cdd94f68e1a20b7306c6cf4c7440bcfee214370
Harshit Sharma <sharma.harshit0059@gmail.com>
2025-08-15 15:00:21 +0530

    Add problem statement and solutions for "Power of Four" (Leetcode 342)

---
+++ Leetcode/342 -Power of Four/solution.js
@@ -0,0 +1,3 @@
+var isPowerOfFour = function(n) {
+    return n > 0 && ((n & (n - 1)) === 0) && ((n & 0x55555555) !== 0);
+};
---
+++ Leetcode/342 -Power of Four/solution.py
@@ -0,0 +1,2 @@
+def isPowerOfFour(self, n: int) -> bool:
+    return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
