70d1bf602b19319b126be1620cefefbda6930bc6
Harshit Sharma <sharma.harshit0059@gmail.com>
2025-09-11 13:56:35 +0530

    Add problem statement, solutions, and explanations for "Unique Paths II" (Leetcode 63)

---
+++ Leetcode/63 - Unique Paths II/solution.cpp
@@ -0,0 +1,26 @@
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    if (obstacleGrid[0][0] == 1) return 0;
+    
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
+    obstacleGrid[0][0] = 1;
+
+    for (int i = 1; i < m; i++) {
+        obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1) ? 1 : 0;
+    }
+    for (int j = 1; j < n; j++) {
+        obstacleGrid[0][j] = (obstacleGrid[0][j] == 0 && obstacleGrid[0][j-1] == 1) ? 1 : 0;
+    }
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[i][j] == 1) {
+                obstacleGrid[i][j] = 0;
+            } else {
+                obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+            }
+        }
+    }
+
+    return obstacleGrid[m-1][n-1];
+}
---
+++ Leetcode/63 - Unique Paths II/solution.py
@@ -0,0 +1,22 @@
+def uniquePathsWithObstacles(obstacleGrid: List[List[int]]) -> int:
+    if obstacleGrid[0][0] == 1:
+        return 0
+
+    m: int = len(obstacleGrid)
+    n: int = len(obstacleGrid[0])
+    obstacleGrid[0][0] = 1
+
+    for i in range(1, m):
+        obstacleGrid[i][0] = 1 if obstacleGrid[i][0] == 0 and obstacleGrid[i - 1][0] == 1 else 0
+
+    for j in range(1, n):
+        obstacleGrid[0][j] = 1 if obstacleGrid[0][j] == 0 and obstacleGrid[0][j - 1] == 1 else 0
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if obstacleGrid[i][j] == 1:
+                obstacleGrid[i][j] = 0
+            else:
+                obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+
+    return obstacleGrid[m - 1][n - 1]
